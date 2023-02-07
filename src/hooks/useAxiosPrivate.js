5903765631dd8df6b8cb933e5860cceec3bcbedb
Tae Hyeon Jo <hillskuti@naver.com>
2023-02-07 23:56:17 +0900

    private axios

---
+++ src/hooks/useAxiosPrivate.js
@@ -0,0 +1,9 @@
+import { axiosPrivate } from '../api/axios';
+import { useEffect, useRef } from 'react';
+import useRefreshToken from './useRefreshToken';
+import useAuth from './useAuth';
+
+const useAxiosPrivate = () => {
+  const refresh = useRefreshToken();
+  const { auth } = useAuth();
+};
