f2ec7f9755a00c0e5681b5bafcc38118512005b9
Tae Hyeon Jo <hillskuti@naver.com>
2023-02-08 23:42:10 +0900

    AxiosPrivate2

--- src/hooks/useAxiosPrivate.js
+++ src/hooks/useAxiosPrivate.js
@@ -6,4 +6,22 @@ import useAuth from './useAuth';
 const useAxiosPrivate = () => {
   const refresh = useRefreshToken();
   const { auth } = useAuth();
+
+  useEffect(() => {
+    const responseIntercept = axiosPrivate.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const prevRequest = error?.config;
+        if (error?.response.status === 403 && !prevRequest?.sent) {
+          prevRequest.sent = true;
+          const newAccessToken = await refresh();
+          prevRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+        }
+      }
+    );
+  }, [auth, refresh]);
+
+  return axiosPrivate;
 };
+
+export default useAxiosPrivate;
